f7c8914ca8f4a95314de8a014b5149319a6144f1
Kaylee Trevino <kayleetrevino@Kaylees-MBP.lan>
2020-10-28 10:26:23 -0500

    Added basefiles for testing

---
+++ templates/test_home.py
@@ -0,0 +1,42 @@
+import unittest
+
+from selenium import webdriver
+
+class TestHome(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Safari()
+        # self.driver.get(r"file:///Users/kayleetrevino/Desktop/SoftwareLab/FitHubProject/TeamA13/templates/homepage.html")
+        self.driver.get("http://localhost:8080/")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_start_at_home_page(self):
+        self.assertEqual("Fithub Homepage", self.driver.title)
+
+    def test_exercise_carousel_link(self):
+        link = self.driver.find_element_by_link_text("See Exercises")
+        print(link)
+        print(self.driver.current_url)
+        link.click()
+        print(self.driver.current_url)
+
+        self.assertEqual("Exercises Model Page", self.driver.title)
+
+    # def test_equipment_carousel_link():
+    #     btn = self.driver.find_element_by_id("equipmentCarouselBtn")
+    #     btn.click()
+
+    #     assertEquals("Equipment Model Page", driver.getTitle())
+
+    # def test_channel_carousel_link():
+    #     btn = self.driver.find_element_by_id("channelCarouselBtn")
+    #     btn.click()
+
+    #     assertEquals("Youtube Channel Model Page", driver.getTitle())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
