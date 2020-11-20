acaf216f776462594113ff62a2ae0323b88de742
Kaylee Trevino <kayleetrevino@Kaylees-MBP.lan>
2020-11-19 23:02:15 -0600

    Added a bit of styling

---
+++ static/search_test.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+const { describe } = require("yargs");
+const { isMainThread } = require("worker_threads");
+
+describe("Testing Search Bar", function () {
+  describe("#searchBar", function () {
+    it("should get some value", () => {});
+  });
+});
