636cf2db4c2c6f5a55dbcf5dbb242f7dafb0d232
Christopher S <schrodingersfridge@utexas.edu>
2020-11-20 22:35:06 -0600

    Finalized testing

--- test_js.py
+++ test_js.py
@@ -2,9 +2,11 @@ import random
 import unittest
 
 from selenium import webdriver
-
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
 
 # NOTE: MUST HAVE SERVER RUNNING IN INCOGNITO WINDOW BEFORE RUNNING TESTS
+
 class JavaScriptTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox(executable_path=r"C:\Users\chand\GIT_STUFF_GOES_HERE\EE461L\geckodriver.exe")
@@ -33,7 +35,7 @@ class JavaScriptTest(unittest.TestCase):
         # fill out form with a few checkmarks
         self.driver.find_element_by_id("openNavButton").click()
         filterForm = self.driver.find_element_by_id("equipmentsFilterForm")
-        checkboxes = filterForm.find_elements_by_tag_name("label")
+        checkboxes = filterForm.find_elements_by_tag_name("input")
         print("Checkboxes found: " + str(checkboxes))
         filterFormState1 = []
         alternator = 0
@@ -42,6 +44,7 @@ class JavaScriptTest(unittest.TestCase):
             if alternator == 1 or alternator == 5:
                 checkbox.click()
                 filterFormState1.append(checkbox)
+                print(str(checkbox.is_selected()))
         self.driver.find_element_by_id("filterButton").click()
 
         # exit page and go back, save form state as another text
@@ -50,13 +53,13 @@ class JavaScriptTest(unittest.TestCase):
         self.driver.implicitly_wait(50000)
         self.driver.find_element_by_id("openNavButton").click()
         filterForm = self.driver.find_element_by_id("equipmentsFilterForm")
-        checkboxes = filterForm.find_elements_by_tag_name("label")
+        checkboxes = filterForm.find_elements_by_tag_name("input")
         print("Checkboxes found: " + str(checkboxes))
         alternator = 0
         for checkbox in checkboxes:
             alternator = alternator + 1
             if alternator == 1 or alternator == 5:
-                self.assertTrue(checkbox.is_enabled())
+                self.assertTrue(checkbox.is_selected())
 
     def test_resetCheckBoxState(self):
         self.driver.get("http://localhost:8080/equipment/1")
@@ -64,42 +67,63 @@ class JavaScriptTest(unittest.TestCase):
         # fill out form with a few checkmarks
         self.driver.find_element_by_id("openNavButton").click()
         filterForm = self.driver.find_element_by_id("equipmentsFilterForm")
-        checkboxes = filterForm.find_elements_by_tag_name("label")
+        checkboxes = filterForm.find_elements_by_tag_name("input")
         print("Checkboxes found: " + str(checkboxes))
         alternator = 0
         for checkbox in checkboxes:
-            alternator = alternator + 1
             if alternator == 1 or alternator == 5:
                 checkbox.click()
+            alternator = alternator + 1
         self.driver.find_element_by_id("filterButton").click()
 
         # reset checkmarks
         self.driver.get("http://localhost:8080/equipment/1")
-        self.driver.implicitly_wait(50000)
-        self.driver.find_element_by_id("openNavButton").click()
-        self.driver.find_element_by_id("resetButton").click()
-        self.driver.implicitly_wait(50000)
+        self.driver.implicitly_wait(50)
+        self.driver.find_element_by_id("resetBtn").click()
+        self.driver.implicitly_wait(50)
         self.driver.find_element_by_id("openNavButton").click()
         filterForm = self.driver.find_element_by_id("equipmentsFilterForm")
-        checkboxes = filterForm.find_elements_by_tag_name("label")
+        checkboxes = filterForm.find_elements_by_tag_name("input")
+        self.driver.implicitly_wait(250)
         print("Checkboxes found: " + str(checkboxes))
         alternator = 0
         for checkbox in checkboxes:
             print(alternator)
-            self.assertFalse(checkbox.is_enabled())
+            self.assertFalse(checkbox.is_selected())
+            alternator = alternator + 1
 
-
-    def resetSearchSort(self):
+    def test_resetSearchSort(self):
         self.driver.get("http://localhost:8080/equipment/1")
 
+        # get first results before anything
+        object1_BEFORE = self.driver.find_element_by_id("name_0").text
+        object2_BEFORE = self.driver.find_element_by_id("name_1").text
+        print("object1_BEFORE = " + object1_BEFORE)
+        print("object2_BEFORE = " + object2_BEFORE)
+
         # fill out search
-        self.driver.find_element_by_id("searchBar").send_keys("g")
-        searchResults = self.driver.find_element_by_id("menuItems")
-        searchResults[0].click()
+        self.driver.find_element_by_id("searchBar").send_keys("ge")
+        self.driver.find_element_by_id("searchBar").send_keys(Keys.RETURN)
 
+        # sort by price
+        sortDropDown = Select(self.driver.find_element_by_id("equipmentsSortCriteriaMenu"))
+        sortDropDown.select_by_value("price")
+        self.driver.find_element_by_id("descendBtn").click()
+        object1_DURING = self.driver.find_element_by_id("name_0").text
+        object2_DURING = self.driver.find_element_by_id("name_1").text
+        print("object1_BEFORE = " + object1_DURING)
+        print("object2_BEFORE = " + object2_DURING)
 
-
-    def test_clearLocalStorage(self):
+        # reset it all, check if was resetted
+        self.driver.find_element_by_id("resetBtn").click()
+        object1_AFTER = self.driver.find_element_by_id("name_0").text
+        object2_AFTER = self.driver.find_element_by_id("name_1").text
+        print("object1_BEFORE = " + object1_AFTER)
+        print("object2_BEFORE = " + object2_AFTER)
+        self.assertEquals(str(object1_BEFORE), str(object1_AFTER))
+        self.assertEquals(str(object2_BEFORE), str(object2_AFTER))
+        self.assertNotEquals(str(object1_DURING), str(object1_AFTER))
+        self.assertNotEquals(str(object2_DURING), str(object2_AFTER))
 
 
 if __name__ == '__main__':
